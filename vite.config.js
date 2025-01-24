9beabcc7716686ecc99c5aabbacda7cc604e0af4
nghia3anp3 <bapngan892003@gmail.com>
2025-01-24 17:18:06 +0700

    run

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    open: true,
-  },
-  build: {
-    outDir: "dist",
-  },
-  // Cấu hình fallback
-  resolve: {
-    alias: {
-      "@": "/src",
-    },
-  },
-  preview: {
-    port: 3000,
-  },
-  // Cấu hình SPA fallback
-  esbuild: {
-    jsx: "automatic",
+    port: 5173, // Cổng cho môi trường development
   },
 });
